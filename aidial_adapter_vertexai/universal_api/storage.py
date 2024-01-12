5f3586e37ef0b6f73a01f753df3acd399f50486c
Anton Dubovik <anton_dubovik@epam.com>
2024-01-12 14:56:00 +0000

    fix: propagate api-key instead of authorization

--- aidial_adapter_vertexai/universal_api/storage.py
+++ aidial_adapter_vertexai/universal_api/storage.py
@@ -7,7 +7,6 @@ from typing import Mapping, Optional, TypedDict
 from urllib.parse import urljoin
 
 import aiohttp
-from aiohttp import hdrs
 
 from aidial_adapter_vertexai.utils.auth import Auth
 from aidial_adapter_vertexai.utils.log_config import app_logger as logger
@@ -126,7 +125,7 @@ def create_file_storage(
     if DIAL_URL is None:
         return None
 
-    auth = Auth.from_headers(hdrs.AUTHORIZATION, headers)
+    auth = Auth.from_headers("api-key", headers)
     if auth is None:
         logger.debug(
             "The request doesn't have required headers to use the DIAL file storage. "
