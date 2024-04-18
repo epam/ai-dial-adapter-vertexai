b3a06bed7f92d6750edfcc54d5977bb3da2efee7
Anton Dubovik <anton_dubovik@epam.com>
2024-04-18 14:49:00 +0100

    fix: documented modal capabilities of Gemini more thoroughly

--- aidial_adapter_vertexai/chat/gemini/processors.py
+++ aidial_adapter_vertexai/chat/gemini/processors.py
@@ -12,8 +12,16 @@ from aidial_adapter_vertexai.chat.gemini.processor import (
 # Prompt design: https://cloud.google.com/vertex-ai/generative-ai/docs/multimodal/design-multimodal-prompts
 # Pricing: https://cloud.google.com/vertex-ai/generative-ai/pricing
 
+# Text/Code processing:
+# 1.0: max_total_tokens: 16384, max_completion_tokens: 2048
+# 1.5: max_total_tokens ~: 1M, max_completion_tokens: not specified
 
-# Tokens per image: 258. count_tokens API call takes this into account.
+
+# Image processing:
+# 1.0:
+#  * max number of images: 16
+#  * Tokens per image: 258. count_tokens API call takes this into account.
+# 1.5: max number of images: 3000
 def get_image_processor(
     max_count: int,
     init_validator: InitValidator | None = None,
@@ -30,8 +38,10 @@ def get_image_processor(
     )
 
 
-# The maximum number of hours of audio per prompt is approximately 8.4 hours,
-# or up to 1 million tokens (not checked).
+# Audio processing
+# 1.0: not supported
+# 1.5: the maximum number of hours of audio per prompt is approximately 8.4 hours,
+#      or up to 1 million tokens (not checked).
 def get_audio_processor(
     init_validator: InitValidator | None = None,
 ) -> AttachmentProcessor:
@@ -41,8 +51,11 @@ def get_audio_processor(
     )
 
 
+# PDF processing
+# 1.0: max number of PDF pages: 16
+# 1.5: max number of PDF pages: 3000
 # The maximum file size for a PDF is 50MB (not checked).
-# PDFs are treated as images, so a single page of a PDF is treated as one image.
+# PDF pages are treated as individual images.
 def get_pdf_processor(
     max_page_count: int,
     init_validator: InitValidator | None = None,
@@ -54,17 +67,18 @@ def get_pdf_processor(
     )
 
 
-# For Gemini 1.5 Pro (Preview) only, the audio track is encoded with video frames.
-# The audio track is also broken down into 1-second trunks that
-# each accounts for 32 tokens.
-# The video frame and audio tokens are interleaved together with their timestamps. # The timestamps are represented as 7 tokens.
-# Maximum video length when it includes audio is approximately 50 minutes (not checked).
-# The maximum video length for video without audio is 1 hour (not checked).
-
-
-# Audio in the video is ignored.
-# Videos are sampled at 1fps. Each video frame accounts for 258 tokens.
-# The video is automatically truncated to the first two minutes.
+# Video processing
+# 1.0:
+#   * Maximum video length is 2 minutes (not checked)
+#   * The maximum number of videos: 1
+#   * Audio in the video is ignored.
+#   * Videos are sampled at 1fps. Each video frame accounts for 258 tokens.
+# 1.5:
+#   * the audio track is encoded with video frames.
+#   * The audio track is also broken down into 1-second trunks that each accounts for 32 tokens.
+#   * The video frame and audio tokens are interleaved together with their timestamps. #   * The timestamps are represented as 7 tokens.
+#   * Maximum video length when it includes audio is approximately 50 minutes (not checked)
+#   * The maximum video length for video without audio is 1 hour (not checked)
 def get_video_processor(
     max_count: int,
     init_validator: InitValidator | None = None,
