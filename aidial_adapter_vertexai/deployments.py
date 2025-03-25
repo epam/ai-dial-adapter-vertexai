0528c2f3949bc3243030cf5f0718d497c81d54cb
Anton Dubovik <anton_dubovik@epam.com>
2025-03-25 15:14:22 +0100

    feat: add Claude 3.7 Sonnet (#203)

--- aidial_adapter_vertexai/deployments.py
+++ aidial_adapter_vertexai/deployments.py
@@ -32,6 +32,7 @@ class ChatCompletionDeployment(Enum):
     CLAUDE_3_OPUS = "claude-3-opus@20240229"
     CLAUDE_3_5_SONNET = "claude-3-5-sonnet@20240620"
     CLAUDE_3_HAIKU = "claude-3-haiku@20240307"
+    CLAUDE_3_7_SONNET = "claude-3-7-sonnet@20250219"
 
 
 ClaudeDeployment = Literal[
@@ -40,6 +41,7 @@ ClaudeDeployment = Literal[
     ChatCompletionDeployment.CLAUDE_3_OPUS,
     ChatCompletionDeployment.CLAUDE_3_5_SONNET,
     ChatCompletionDeployment.CLAUDE_3_HAIKU,
+    ChatCompletionDeployment.CLAUDE_3_7_SONNET,
 ]
 
 GeminiDeployment = Literal[
