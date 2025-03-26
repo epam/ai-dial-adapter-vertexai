04de61f8e60af45c03a4d926873aad2fc3224e4a
Anton Dubovik <anton_dubovik@epam.com>
2025-03-26 11:39:34 +0000

    feat: add project to upstream configuration

--- aidial_adapter_vertexai/app_config.py
+++ aidial_adapter_vertexai/app_config.py
@@ -7,18 +7,18 @@ from google.genai.client import Client as GenAIClient
 from aidial_adapter_vertexai.utils.env import get_env
 
 DEFAULT_REGION = get_env("DEFAULT_REGION")
-PROJECT_ID = get_env("GCP_PROJECT_ID")
+DEFAULT_PROJECT = get_env("GCP_PROJECT_ID")
 
 
 def init_vertex_ai():
-    vertexai.init(project=PROJECT_ID, location=DEFAULT_REGION)
+    vertexai.init(project=DEFAULT_PROJECT, location=DEFAULT_REGION)
 
 
 @cache
-def get_genai_client(location: str) -> GenAIClient:
-    return GenAIClient(vertexai=True, project=PROJECT_ID, location=location)
+def get_genai_client(project: str, location: str) -> GenAIClient:
+    return GenAIClient(vertexai=True, project=project, location=location)
 
 
 @cache
-def get_anthropic_client(region: str) -> AsyncAnthropicVertex:
-    return AsyncAnthropicVertex(project_id=PROJECT_ID, region=region)
+def get_anthropic_client(project: str, region: str) -> AsyncAnthropicVertex:
+    return AsyncAnthropicVertex(project_id=project, region=region)
