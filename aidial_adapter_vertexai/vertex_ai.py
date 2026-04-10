902963af48116734066563db613793b76c457d80
Alex Chernyshov <alexfoxalt@gmail.com>
2026-04-10 17:33:57 +0300

    feat: migrate embeddings to genai lib

--- aidial_adapter_vertexai/vertex_ai.py
+++ aidial_adapter_vertexai/vertex_ai.py
@@ -1,15 +1,7 @@
-from vertexai.preview.language_models import TextEmbeddingModel
 from vertexai.vision_models import MultiModalEmbeddingModel
 
 from aidial_adapter_vertexai.utils.cache import cache
 from aidial_adapter_vertexai.utils.concurrency import make_single_thread_async
-
-
-@cache()
-async def get_text_embedding_model(model_id: str) -> TextEmbeddingModel:
-    return await make_single_thread_async(
-        TextEmbeddingModel.from_pretrained, model_id
-    )
 
 
 @cache()
