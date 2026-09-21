574535ffa00ff107a0ef0e5875f726665f72d870
Anton Dubovik <anton_dubovik@epam.com>
2026-09-21 17:43:05 +0100

    refactor: inline mount_anthropic_passthrough

--- aidial_adapter_vertexai/anthropic_passthrough.py
+++ aidial_adapter_vertexai/anthropic_passthrough.py
@@ -1,5 +1,4 @@
-from aidial_adapter_anthropic.passthrough import mount_anthropic_api
-from fastapi import FastAPI, Request
+from fastapi import Request
 
 from aidial_adapter_vertexai.upstream_config import (
     AnthropicClient,
@@ -7,10 +6,6 @@ from aidial_adapter_vertexai.upstream_config import (
 )
 
 
-async def _get_anthropic_client(request: Request) -> AnthropicClient:
+async def get_anthropic_client(request: Request) -> AnthropicClient:
     upstream_config = parse_upstream_config(request)
     return await upstream_config.get_anthropic_client()
-
-
-def mount_anthropic_passthrough(app: FastAPI, path: str):
-    mount_anthropic_api(app, _get_anthropic_client, path=path)
